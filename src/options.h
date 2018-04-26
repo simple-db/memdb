0c4ef8c3738845ac58a673f21582809471c6f948
wtzhuque <w.t.zhuque@gmail.com>
2018-04-27 00:37:25 +0800

    add options

--- src/options.h
+++ src/options.h
@@ -3,15 +3,4 @@
 /* Email: wtzhuque@163.com       */
 /*********************************/
 
-namespace codu {
 
-class PhotonDB {
-public:
-    PhotonDB();
-
-    ~PhotonDB();
-
-    int init();
-}; // class PhotonDB
-
-} // namespace codu
